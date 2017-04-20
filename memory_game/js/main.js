01a9350f21547c2fc0c03750892556f6007bb173
Justin Rhoads <justinrhoads@Justins-MacBook-Pro.local>
2017-04-20 09:19:29 -0500

    created game logic

---
+++ memory_game/js/main.js
@@ -0,0 +1,24 @@
+console.log("Up and Running!");
+var cards = ["queen", "queen", "king", "king"];
+var cardsInPlay = [];
+
+var checkForMatch = function () {
+	if(cardsInPlay.length === 2) {
+	if (cardsInPlay[0] === cardsInPlay[1]) {
+		alert("You found a match!")
+	} else {
+		alert("Sorry, try again.");
+	}
+	}
+};
+
+var flipCard = function (cardId) {
+console.log("User flipped " + cards[cardId]);
+cardsInPlay.push(cards[cardId]);
+checkForMatch();
+}
+flipCard(0);
+flipCard(2);
+
+
+
