807d0b32d4caa9f2d0507ddacb11ebcee6418599
Justin Rhoads <rhoadsjustin@gmail.com>
2017-04-24 08:25:26 -0500

    created game logic

--- memory_game/js/main.js
+++ memory_game/js/main.js
@@ -1,4 +1,3 @@
-console.log("Up and Running!");
 var cards = [ {
 	rank: "queen",
 	suit: "hearts",
@@ -24,6 +23,7 @@ var cards = [ {
 var cardsInPlay = [];
 
 var checkForMatch = function (){
+  document.getElementsByTagName('src')[0].setAttribute('cardImage', console.log(cards[cardId].cardImage));
 if(cardsInPlay.length === 2) {
 	if (cardsInPlay[0] === cardsInPlay[1]) {
 		alert("You found a match!")
@@ -33,13 +33,23 @@ if(cardsInPlay.length === 2) {
 	}
 };
 
-var flipCard = function (cardId) {
-	console.log("User flipped " + cards[cardId].rank);
+var flipCard = function () {
+  this.getAttribute('data-id')
+  console.log("User flipped " + cards[cardId].rank);
 	cardsInPlay.push(cards[cardId].rank);
 	console.log(cards[cardId].cardImage);
 	console.log(cards[cardId].suit);
 	checkForMatch();
 };
-flipCard(0);
-flipCard(2);
 
+var createBoard = function () {
+  for (var i  = 0;  i > cards.length; i++) {
+    var cardElement = document.createElement('img');
+    cardElement.setAttribute('src', 'images/back.png');
+    cardElement.setAttribute('data-id' , i);
+    this.addEventListener('click', flipCard);
+    document.getElementById('game-board').appendChild(cardElement);
+  }
+};
+
+createBoard();
